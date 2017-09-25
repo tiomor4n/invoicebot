f4088070622e8b663a9affadc855b98b50f152eb
tiomor4n <tiomor4n@gmail.com>
2017-09-26 01:36:40 +0800

    utility

--- inbot/utility.py
+++ inbot/utility.py
@@ -143,3 +143,19 @@ def isint(value):
         return True
     except ValueError:
         return False
+
+def writelog(logstr):
+    import logging
+    logger = logging.getLogger('mylogger')  
+    logger.setLevel(logging.DEBUG)  
+    fh = logging.FileHandler(fileroute + '\\test.log')  
+    fh.setLevel(logging.DEBUG)  
+    #ch = logging.StreamHandler()  
+    #ch.setLevel(logging.DEBUG)  
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  
+    fh.setFormatter(formatter)  
+    #ch.setFormatter(formatter)  
+    logger.addHandler(fh)  
+    #logger.addHandler(ch)  
+    logger.info(logstr) 
+    Logger = None
