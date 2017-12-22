a391d59d0796c26b71e8eb5d566225b2655ba8ec
tiomor4n <tiomor4n@gmail.com>
2017-12-23 01:23:22 +0800

    change debug

--- invoicebot/settings.py
+++ invoicebot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')d*$lagk&uycbofxb7sm#a96agh(*h^eykx_v#3ikukkr#9+0_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
